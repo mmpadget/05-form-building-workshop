f6f3103e06ca79348a1f959c4aa050913e60d1d7
Michael Padget <mpadget@comcast.net>
2017-03-19 17:04:22 -0700

    Completed fourth to-do: hold articles, empty articles

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -86,7 +86,9 @@ articleView.initNewArticlePage = function() {
 articleView.create = function() {
   // TODO: Set up a var to hold the new article we are creating.
   // Clear out the #articles element, so we can put in the updated preview
+  var newArticle;
 
+  $('#articles').empty();
 
   // TODO: Instantiate an article based on what's in the form fields:
 
